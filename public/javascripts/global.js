1611692438ed7d3901280e254725bbc1b99b33e7
EC2 Default User <ec2-user@ip-172-31-47-161.ec2.internal>
2014-09-23 19:36:35 +0000

    Change map from Google Maps API to Leaflet

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,37 +1,56 @@
-// DOM Ready =============================================================
-$(document).ready(function() {
+// Document ready
+$(document).ready(initialize);
 
-  // Populate the map on initial page load
-  initialize();
-});
 
-// Functions =============================================================
-
-var fork_green  = '/images/fork_green.png';
-var fork_yellow = '/images/fork_yellow.png';
-var fork_red    = '/images/fork_red.png';
+// Data ==================================================================
 
 var map = null;
 var markers = [];
-var infowindows = [];
-var markerCluster = null;
+var popupwindows = [];
+
+// Restaurant layer group
+//var restaurants = new L.LayerGroup();
+var restaurants = new L.MarkerClusterGroup();
 
 
 // Functions =============================================================
 
+function initialize()
+{
+	map = CreateMap();
+
+	var parent = L.DomUtil.create('div', 'custom-button-group');
+	map.addControl(new FilterControl({'text': 'Below 60', 'dataUrl': '/restaurants/50s', 'onClick': onClick50s, 'parent': parent}));
+	map.addControl(new FilterControl({'text': '60 - 69', 'dataUrl': '/restaurants/60s', 'onClick': onClick60s, 'parent': parent}));
+	map.addControl(new FilterControl({'text': '70 - 79', 'dataUrl': '/restaurants/70s', 'onClick': onClick70s, 'parent': parent}));
+	map.addControl(new FilterControl({'text': '80 - 89', 'dataUrl': '/restaurants/80s', 'onClick': onClick80s, 'parent': parent}));
+	map.addControl(new FilterControl({'text': '90 - 99', 'dataUrl': '/restaurants/90s', 'onClick': onClick90s, 'parent': parent}));
+	map.addControl(new FilterControl({'text': '100', 'dataUrl': '/restaurants/100', 'onClick': onClick100, 'parent': parent}));
+	map.addControl(new FilterControl({'text': 'All (slow)', 'dataUrl': '/restaurants/all', 'onClick': onClickAll, 'parent': parent}));
+}   
+
 function CreateMap()
 {
-	// The center of Austin, Texas
-	var mapcenter = new google.maps.LatLng(30.2500, -97.7500);
-	var zoom      = 12;
-	var map_id    = 'map-canvas';
+	var center = new L.LatLng(30.2500, -97.7500);
+	var zoom   = 12;
 
-	var mapOptions =
-	{
-		center: mapcenter,
-		zoom: zoom,
-	};
-	var new_map = new google.maps.Map(document.getElementById(map_id), mapOptions);
+	var new_map = L.map(
+		'map',
+		{
+			center: center,
+			zoom: zoom,
+			maxZoom: 18,
+			layers: [restaurants]
+		});
+
+	L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'examples.map-i875mjb7'
+	}).addTo(new_map);
+	
 	return new_map;
 }
 
@@ -40,23 +59,12 @@ function CreateMarker(data)
 	//
 	// Create a restaurant marker
 	//
-	var image = fork_green;
-	if (data.score < 75)
-	{
-		image = fork_red;
-	}
-
-	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(data.address.latitude, data.address.longitude),
-		map: map,
-		title: data.restaurant_name,
-		icon: image
-	});
+	var marker = L.marker([data.address.latitude, data.address.longitude]).addTo(restaurants);
 	markers.push(marker);
 	return marker;
 }
 
-function CreateInfoWindow(data)
+function CreatePopupWindow(data, marker)
 {
 	//
 	// Create an info window
@@ -66,60 +74,42 @@ function CreateInfoWindow(data)
 		'<h4 id="firstHeading" class="firstHeading">' + data.restaurant_name + '</h4>' +
 		'<b>Inspection Scores:</b>' +
 		'<br><b>' + data.score + '</b> (' + locale_date.toLocaleDateString() + ')';
-
-	var infowindow = new google.maps.InfoWindow({
-		content: contentString,
-		maxWidth: 600
-	});
-	infowindows.push(infowindow);
-	return infowindow;
+	marker.bindPopup(contentString);
+	return marker.getPopup();
 }
 
-function UpdateInfoWindow(data, infowindow)
+function UpdatePopupWindow(data, popup)
 {
 	// Update the info window with historical inspection scores
 	var locale_date = new Date(data.inspection_date * 1000);
-	var contentString = infowindow.getContent() +
+	var contentString = popup.getContent() +
 		'<br><b>' + data.score + '</b> (' + locale_date.toLocaleDateString() + ')';
-	infowindow.setContent(contentString);
-}
-
-function CreateClusters()
-{
-	//
-	// Cluster management
-	//
-	markerCluster = new MarkerClusterer(map, markers, { maxZoom: 15 });
+	popup.setContent(contentString);
 }
 
 function ClearMarkers()
 {
-	for (var i = 0; i < markers.length; i++)
-	{
-		markers[i].setMap(null);
-	}
+	// Remove the layer containing the markers
+	map.removeLayer(restaurants);
 	markers = [];
+
+	// Recreate the layer so markers can be re-added
+	restaurants = new L.MarkerClusterGroup();
 }
 
-function ClearClusters()
+function ClearPopupWindows()
 {
-	if (markerCluster != null)
+	for (var i = 0; i < popupwindows.length; i++)
 	{
-		markerCluster.clearMarkers();
-		markerCluster = null;
+		popupwindows[i].addTo(null);
 	}
-}
-
-function ClearInfoWindows()
-{
-	infowindows = [];
+	popupwindows = [];
 }
 
 function ClearMap()
 {
 	ClearMarkers();
-	ClearClusters();
-	ClearInfoWindows();
+	ClearPopupWindows();
 }
 
 function PopulateMarkers(data_url)
@@ -129,7 +119,7 @@ function PopulateMarkers(data_url)
 	$.getJSON( data_url, function( data )
 	{
 		var prev_facility_id = 0;
-		var current_infowindow = null;
+		var current_popupwindow = null;
 
 		ClearMap();
 
@@ -140,120 +130,79 @@ function PopulateMarkers(data_url)
 				// New facility id encountered.
 				// Create marker with most recent information
 				var marker = CreateMarker(this);
-				var infowindow = CreateInfoWindow(this);
-				current_infowindow = infowindow;
-
-				google.maps.event.addListener(marker, 'click', function() {
-						infowindow.open(map, marker);
-				});
+				var popupwindow = CreatePopupWindow(this, marker);
+				current_popupwindow = popupwindow;
 
 				prev_facility_id = this.facility_id;
 			}
 			else
 			{
 				// Update info window with previous inspection scores
-				UpdateInfoWindow(this, current_infowindow);
+				UpdatePopupWindow(this, current_popupwindow);
 			}
 		});
+		map.addLayer(restaurants);
+	});
+}
+
+// Button controls for filtering
+var FilterControl = L.Control.extend({
+	options: {
+		position: 'topright'
+	},
+
+	initialize: function(options) {
+		this._button = {};
+		this.setButton(options);
+		this._container = options.parent;
+	},
+
+	onAdd: function(map) {
+		this._map = map;
+		this._update();
+		return this._container;
+	},
+
+	setButton: function(options) {
+		var button = {
+			'text'    : options.text,
+			'dataUrl' : options.dataUrl,
+			'onClick' : options.onClick,
+			'parent'  : options.parent
+		};
+		this._button = button;
+	},
 	
-		CreateClusters();
-	});
-}
+	_update: function() {
+		this._container.style.padding = '5px';
 
-function FilterControl(controlDiv, buttonText, data_url)
-{
-	controlDiv.style.padding = '5px';
+		var controlUI = document.createElement('div', 'custom-control');
+		controlUI.style.backgroundColor = 'white';
+		controlUI.style.borderStyle = 'solid';
+		controlUI.style.borderWidth = '1px';
+		controlUI.style.cursor = 'pointer';
+		controlUI.style.textAlign = 'center';
+		controlUI.title = 'Click to display ' + this._button.text + ' scorers';
+		this._container.appendChild(controlUI);
 
-	var controlUI = document.createElement('div');
-	controlUI.style.backgroundColor = 'white';
-	controlUI.style.borderStyle = 'solid';
-	controlUI.style.borderWidth = '1px';
-	controlUI.style.cursor = 'pointer';
-	controlUI.style.textAlign = 'center';
-	controlUI.title = 'Click to display ' + buttonText + ' scorers';
-	controlDiv.appendChild(controlUI);
+		var controlText = document.createElement('div', 'custom-control-text');
+		controlText.style.fontFamily = 'Arial,sans-serif';
+		controlText.style.fontSize = '12px';
+		controlText.style.paddingLeft = '4px';
+		controlText.style.paddingRight = '4px';
+		controlText.innerHTML = '<b>' + this._button.text + '</b>';
+		controlUI.appendChild(controlText);
+		
+		controlUI.addEventListener('click', this._button.onClick, true);
+	},
+});
 
-	var controlText = document.createElement('div');
-	controlText.style.fontFamily = 'Arial,sans-serif';
-	controlText.style.fontSize = '12px';
-	controlText.style.paddingLeft = '4px';
-	controlText.style.paddingRight = '4px';
-	controlText.innerHTML = '<b>' + buttonText + '</b>';
-	controlUI.appendChild(controlText);
-
-	google.maps.event.addDomListener(controlUI, 'click', function() {
-		PopulateMarkers(data_url);
-	});
-}
-
-
-function initialize()
-{
-	map = CreateMap();
-
-	//
-	// Top scorers button
-	//
-	var score100ControlDiv = document.createElement('div');
-	var score100Control = new FilterControl(score100ControlDiv, '100', '/restaurants/100');
-
-	score100ControlDiv.index = 1;
-	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(score100ControlDiv);
-
-	//
-	// Above 90 button
-	//
-	var score90ControlDiv = document.createElement('div');
-	var score90Control = new FilterControl(score100ControlDiv, '90 - 99', '/restaurants/90s');
-
-	score90ControlDiv.index = 1;
-	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(score90ControlDiv);
-
-	//
-	// Above 80 button
-	//
-	var score80ControlDiv = document.createElement('div');
-	var score80Control = new FilterControl(score100ControlDiv, '80 - 89', '/restaurants/80s');
-
-	score80ControlDiv.index = 1;
-	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(score80ControlDiv);
-
-	//
-	// Above 70 button
-	//
-	var score70ControlDiv = document.createElement('div');
-	var score70Control = new FilterControl(score100ControlDiv, '70 - 79', '/restaurants/70s');
-
-	score70ControlDiv.index = 1;
-	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(score70ControlDiv);
-
-	//
-	// Above 60 button
-	//
-	var score60ControlDiv = document.createElement('div');
-	var score60Control = new FilterControl(score100ControlDiv, '60 - 69', '/restaurants/60s');
-
-	score60ControlDiv.index = 1;
-	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(score60ControlDiv);
-
-	//
-	// Above 50 button
-	//
-	var score50ControlDiv = document.createElement('div');
-	var score50Control = new FilterControl(score100ControlDiv, 'Below 60', '/restaurants/50s');
-
-	score50ControlDiv.index = 1;
-	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(score50ControlDiv);
-
-	//
-	// All data points
-	//
-	var allControlDiv = document.createElement('div');
-	var allControl = new FilterControl(score100ControlDiv, 'All (slow)', '/restaurants/all');
-
-	allControlDiv.index = 1;
-	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(allControlDiv);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+// Button callbacks
+function onClick50s() { PopulateMarkers('/restaurants/50s'); }
+function onClick60s() { PopulateMarkers('/restaurants/60s'); }
+function onClick70s() { PopulateMarkers('/restaurants/70s'); }
+function onClick80s() { PopulateMarkers('/restaurants/80s'); }
+function onClick90s() { PopulateMarkers('/restaurants/90s'); }
+function onClick100() { PopulateMarkers('/restaurants/100'); }
+function onClickAll() { PopulateMarkers('/restaurants/all'); }
 
